a341ebccc0c69a34867bbf9d45323203934c6330
deepathoppil <deepa.thoppil@edu.sait.ca>
2023-10-02 20:26:03 -0600

    set title

--- src/main/java/com/team2/oosdworkshop6/HelloApplication.java
+++ src/main/java/com/team2/oosdworkshop6/HelloApplication.java
@@ -12,7 +12,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("main-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Travel Experts");
+        stage.setTitle("TRAVEL EXPERTS");
         stage.setScene(scene);
         stage.show();
     }
